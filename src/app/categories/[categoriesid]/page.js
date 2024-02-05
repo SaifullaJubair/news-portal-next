57a4b8bd62983e60a049b711e069e6777e0ed1d0
SaifullaJubair <saifullajubair6@gmail.com>
2024-02-06 00:38:47 +0600

    details news shows

--- src/app/categories/[categoriesid]/page.js
+++ src/app/categories/[categoriesid]/page.js
@@ -1,9 +1,49 @@
-import { getAllCategories } from "@/utils/getAllCategories";
+import { getCategoryNews } from "@/utils/getCategoryNews";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
 import React from "react";
 
-const CategoriesId = async () => {
-  const { data: categories } = await getAllCategories();
-  return <div>This is CategoriesId {categories.length}</div>;
+const CategoriesId = async ({ params, searchParams }) => {
+  const data = await getCategoryNews(searchParams.category);
+
+  return (
+    <Box className="mx-4">
+      <Typography>
+        {searchParams.category} have {data.data.length} news
+      </Typography>
+      {data.data?.map((n) => (
+        <Card key={n._id}>
+          <CardActionArea>
+            <CardMedia>
+              <Image
+                src={n.thumbnail_url}
+                alt="news"
+                width={850}
+                height={800}
+              />
+            </CardMedia>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {n.title}
+              </Typography>
+              <Typography color="text.secondary">
+                {n.details.length > 200
+                  ? `${n.details.slice(0)}...`
+                  : n.details}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
+    </Box>
+  );
 };
 
 export default CategoriesId;
--- src/components/ui/CategoryList/CategoryList.js
+++ src/components/ui/CategoryList/CategoryList.js
@@ -1,5 +1,38 @@
-const CategoryList = () => {
-  return <div>total categories data: </div>;
+import { getAllCategories } from "@/utils/getAllCategories";
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Stack,
+  Typography,
+} from "@mui/material";
+import Link from "next/link";
+
+const CategoryList = async () => {
+  const { data: categories } = await getAllCategories();
+  return (
+    <Box className="mt-5 bg-slate-50 py-2 px-5">
+      <Typography variant="h5" sx={{ mb: 3 }}>
+        {" "}
+        Categories
+      </Typography>
+      <Divider />
+      <Stack rowGap={1} sx={{ mt: 3 }}>
+        {categories?.map((category) => (
+          <Button variant="outlined" key={category._id}>
+            <Link
+              href={`/categories/news?category=${category.title.toLowerCase()}`}
+              className="w-full h-full"
+            >
+              {" "}
+              {category.title}
+            </Link>
+          </Button>
+        ))}
+      </Stack>
+    </Box>
+  );
 };
 
 export default CategoryList;
